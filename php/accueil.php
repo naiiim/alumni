cee53c2a75eb25cd0d68fd5af0a4bc18b3738964
naiiim <h.naim@outlook.fr>
2017-01-28 11:44:29 +0100

    fractionnement de la page d'accueil en plusieurs HTML

---
+++ php/accueil.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="../css/normalize.css" />
+    <link rel="stylesheet" href="../css/font.css" />
+    <title>#webdev{height:0.1em}</title>
+  </head>
+  <body>
+
+<?php
+include '../html/header.html';
+include '../html/gif.html';
+include '../html/articles-liste.html';
+include '../html/footer.html';
+?>
